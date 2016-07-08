b47751fd3567563ae291843de89062a4eadcd454
James Williams <jwilliams@pivotal.io>
2016-07-07 22:44:03 -0400

    added rule for beans.xml CDI config file

--- ephemerol/test/test_Scanner.py
+++ ephemerol/test/test_Scanner.py
@@ -9,11 +9,11 @@ class test_Scanner(unittest.TestCase):
         Scanner.load_rules(os.path.join("ephemerol", "test", "rulebase.csv"))
         rb = Scanner.rulebase
 
-        self.assertEqual(66, rb[(rb.app_type == "java")].shape[0])
+        self.assertEqual(67, rb[(rb.app_type == "java")].shape[0])
         self.assertEqual(1, rb[(rb.file_id == "persistence.xml")].shape[0])
         self.assertEqual(6, rb[(rb.refactor_rating == 3)].shape[0])
         self.assertEqual(1, rb[(rb.description == "JPA based ORM")].shape[0])
-        self.assertEqual(53, rb[(rb.file_type == "config")].shape[0])
+        self.assertEqual(54, rb[(rb.file_type == "config")].shape[0])
         self.assertEqual(6, rb[(rb.file_category == "Web Profile")].shape[0])
         self.assertEqual(1, rb[(rb.text_pattern == "import javax.ejb.")].shape[0])
 
@@ -22,9 +22,9 @@ class test_Scanner(unittest.TestCase):
         df = Scanner.scan_archive(os.path.join("ephemerol", "test", "SampleWebApp-master.zip"))
         #print(df)
         self.assertEqual(1, df[(df.refactor_rating == 3)].shape[0])
-        self.assertEqual(5, df[(df.refactor_rating == 1)].shape[0])
+        self.assertEqual(6, df[(df.refactor_rating == 1)].shape[0])
         self.assertEqual(11, df[(df.refactor_rating == 0)].shape[0])
-        self.assertEqual(92, 100 - df.refactor_rating.cumsum().tail(1).item())
+        self.assertEqual(91, 100 - df.refactor_rating.cumsum().tail(1).item())
 
     def test_config_scan(self):
         Scanner.load_rules(os.path.join("ephemerol", "test", "rulebase.csv"))
