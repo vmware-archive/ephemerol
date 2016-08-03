9ac72ea1615b4a99fa76e26f6167be659ed427f4
Chris DeLashmutt <cdelashmutt@pivotal.io>
2016-08-03 15:17:49 -0700

    Adding copyright notice, license and notice files

--- ephemerol/Models.py
+++ ephemerol/Models.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 class ScanItem():
     def __init__(self, app_type, file_category, file_type, file_name, refactor_rating, description, text_pattern):
         self.app_type = app_type
