7e138474551f1e383ed79259291c9f228772781e
James Williams <jwilliams@pivotal.io>
2016-11-27 08:28:20 -0500

    angular2 updates

--- ephemerol/angular2/src/app/rxjs-operators.ts
+++ ephemerol/angular2/src/app/rxjs-operators.ts
@@ -13,22 +13,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This file includes polyfills needed by Angular 2 and is loaded before
-// the app. You can add your own extra polyfills to this file.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/reflect';
+// Statics
+import 'rxjs/add/observable/throw';
 
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
--- ephemerol/angular2/src/app/setup/source-upload.component.ts
+++ ephemerol/angular2/src/app/setup/source-upload.component.ts
@@ -1,6 +1,23 @@
-import { Component, Input } from '@angular/core';
+// Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
+//
+// This program and the accompanying materials are made available under
+// the terms of the under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Component, OnInit } from '@angular/core';
+import { RuleLoadService, Rule } from './rules-load.service';
 @Component({
   selector: 'source-upload',
+  providers: [RuleLoadService],
   template: `
    <div class="row">
         <div class="col-lg-12">
@@ -20,6 +37,18 @@ import { Component, Input } from '@angular/core';
     </div>
   `
 })
-export class SourceUploadComponent {
+export class SourceUploadComponent implements OnInit {
+
+   errorMessage: string;
+   rules: Rule[];
+   mode = 'Observable';
+
+  constructor(
+    private service: RuleLoadService
+  ) {}
+
+  ngOnInit() {
+    this.service.getDefaultRules();
+  }
 
 }
