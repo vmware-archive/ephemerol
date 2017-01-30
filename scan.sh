e97d1bc1aceb32d0afec99cf32418d32e66e6a81
jwilliams-pivotal <jwilliams@pivotal.io>
2017-01-30 18:13:46 -0500

    trimmed concourse scan output

--- scan.sh
+++
@@ -1,8 +0,0 @@
-RULEBASE_PATH=./ephemerol/test/rulebase.yml
-SOURCE_ZIP_PATH=./ephemerol/test/SampleWebApp-master.zip
-MINIMUM_SCAN_INDEX=99
-
-scan_output=`python -m ephemerol $RULEBASE_PATH $SOURCE_ZIP_PATH`
-
-cloud_readiness_index=`echo $scan_output | jq '.scan_stats.cloud_readiness_index'`
-echo "$MINIMUM_SCAN_INDEX>$cloud_readiness_index" | bc
